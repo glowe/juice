50ed32f343a72dcd7155be2834c9503495d84a81
Graham Lowe <graham.lowe@gmail.com>
2008-12-12 15:45:36 -0500

    Removed redundant build path manipulation.

--- tools/compile_web.js
+++ tools/compile_web.js
@@ -15,8 +15,13 @@ sources = settings.args;
 
 lines = [];
 lines.push('(function() {');
-lines.push(juice.compile.read_source_file(settings.make_build_path(template_file)));
+
+print('Attempting to read from ' + template_file);
+lines.push(juice.compile.read_source_file(template_file));
+
+
 lines = lines.concat(juice.map(sources, juice.compile.read_source_file));
 lines.push('})();');
 
+print('Writing to ' + target_file + '\n');
 juice.compile.write_target_file(target_file, lines.join('\n'));
