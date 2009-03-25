5ba4db3fa9e33f2b015d9ddc751f9df7f2db92c0
Graham <glowe@loreal.fearofnumbers.net>
2009-03-25 12:05:40 -0400

    Integrated with new version of v8 interpreter.

--- build/interpreters/v8.js
+++ build/interpreters/v8.js
@@ -1,15 +1,44 @@
 juice.sys.install_interpreter(
-    {basename: sys.basename,
-     canonical_path: sys.realpath,
-     dirname: sys.dirname,
+    {basename: sys.os.basename,
+     canonical_path: sys.os.realpath,
+     dirname: sys.os.dirname,
      exit: quit,
-     file_exists: sys.file_exists,
-     getenv: sys.getenv,
-     read_dir: sys.read_dir,
-     mkdir: sys.mkdir,
-     read_file: sys.read_file,
-     rmdir: sys.rmdir,
-     sha1: sys.sha1,
-     unlink: sys.unlink,
-     write_file: sys.write_file
+     file_exists: function(path) {
+         var file_info;
+         try {
+             file_info = sys.os.stat(path);
+         }
+         catch (e) {
+             if ((e.errno == sys.os.errno.ENOENT) || e.errno != sys.os.errno.ENODIR) {
+                 return false;
+             }
+             throw e;
+         }
+         if (sys.os.S_ISDIR(file_info.st_mode)) {
+             return 'dir';
+         }
+
+         if (sys.os.S_ISREG(file_info.st_mode)) {
+             return 'file';
+         }
+         return juice.error.raise("Unknown file type");
+     },
+     getenv: sys.os.getenv,
+     read_dir: sys.os.listdir,
+     mkdir: sys.os.mkdir,
+     read_file: function(path) {
+         var contents = [], file;
+         if (juice.sys.file_exists(path) !== "file") {
+             throw path + " is not a file";
+         }
+         file = sys.os.fopen(path, "r");
+         while (!sys.os.feof(file)) {
+             contents.push(sys.os.fread(file, 1024));
+         }
+         return contents.join('');
+     },
+     rmdir: sys.os.rmdir,
+     sha1: sys.crypt.sha1,
+     unlink: sys.os.unlink,
+     write_file: sys.os.writefile
     });
