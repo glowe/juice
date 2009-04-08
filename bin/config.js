2fb1e75fa819152a96c9b670b1796b80e2497ba5
Graham <glowe@loreal.fearofnumbers.net>
2009-04-08 11:23:15 -0400

    Deleted some vestigial comments.

--- bin/config.js
+++ bin/config.js
@@ -1,31 +1,8 @@
 
 /*
+ * TODO:
  *
- * TODO: set up a "bin" dir for juice executables
- *
- * 1) command-line arguments:
- *
- * --js=path_to_executable (optional: use "js" in path by default)
- * --with-lib=juice-bar:/path/to/juice-bar/lib
- * --with-lib=jquery:/path/to/jquery/lib
- *
- * 1.5) juice/build/juice-config-nice.js (does what you think)
- *
- * 2) analyze site widgets to find external library dependencies (and do so
- * recursively with external libraries)
- *
- * 2.5) insure that all external libraries can be located, either in the
- * default juice lib path (specified in ~/.juice) or in the path specified on
- * the command-line to this program
- *
- * 3) store settings (library name => path mapping, original command line
- * settings (for nice config), etc.) in ./.juice-config.json
- *
- * NOTES:
- *
- * - During compilation, insure that dependencies on external libraries have
- not changed since the last time we ran configure.
- *
+ * juice/build/juice-config-nice.js (does what you think)
  */
 
 // "JUICE_HOME"
@@ -75,11 +52,8 @@ if (options.help) {
 }
 
 //
-// SETTINGS
-//
-// Possible TODOs:
-// - Confirm that all settings keys are valid.
-// - Check for required settings.
+// SETTINGS TODOS:
+
 // - Maybe even check types of values (e.g. user must point to an object).
 //
 
