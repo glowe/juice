4cca0c49c9c37438bf30b18f287cbb0a5458284d
Graham <glowe@loreal.fearofnumbers.net>
2009-04-14 17:34:13 -0400

    If the build directory gets blown away, our runserver doesn't choke.

--- tools/runserver.py
+++ tools/runserver.py
@@ -7,10 +7,37 @@ import signal
 import sys
 import thread
 import time
+import urlparse
+import posixpath
+import urllib
+
+
+class JuiceHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    document_root = ""
+
+    def translate_path(self, path):
+        """Like SimpleHTTPServer.SimpleHTTPRequestHandler except we
+        aren't restricted to the current directory"""
+
+        # abandon query parameters
+        path = urlparse.urlparse(path)[2]
+        path = posixpath.normpath(urllib.unquote(path))
+        words = path.split('/')
+        words = filter(None, words)
+        path = self.document_root
+        for word in words:
+            drive, word = os.path.splitdrive(word)
+            head, word = os.path.split(word)
+            if word in (os.curdir, os.pardir): continue
+            path = os.path.join(path, word)
+        print path
+        return path
+
 
 def run_server(server_address):
-    SimpleHTTPServer.SimpleHTTPRequestHandler.protocol_version = "HTTP/1.0"
-    httpd = BaseHTTPServer.HTTPServer(server_address, SimpleHTTPServer.SimpleHTTPRequestHandler)
+    JuiceHTTPRequestHandler.protocol_version = "HTTP/1.0"
+
+    httpd = BaseHTTPServer.HTTPServer(server_address, JuiceHTTPRequestHandler)
     hostname, port = httpd.socket.getsockname()
     print "Serving HTTP on", hostname, "port", port, "..."
     httpd.serve_forever()
@@ -19,7 +46,8 @@ def main(args):
     args = args[1:]
     document_root, hostname, port, js_hostname, js_port = args
 
-    os.chdir(document_root)
+    JuiceHTTPRequestHandler.document_root = document_root
+
     base_server_address = (hostname, int(port))
     js_server_address   = (js_hostname, int(js_port))
 
@@ -35,4 +63,4 @@ def main(args):
         pass
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main(sys.argv)
