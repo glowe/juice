c18a1802349ae3eb2942296ac2431712e0ce4be3
Graham <glowe@loreal.fearofnumbers.net>
2009-04-14 16:52:01 -0400

    juice runserver is now able to read the config and use that info to run on the appropriate host and port.

--- bin/runserver.js
+++ bin/runserver.js
@@ -1,3 +1,11 @@
-var docroot = juice.build.final_file_path('');
-print("Running server with docroot: " + docroot);
-juice.sys.system(juice.path_join(juice.home(), "tools/runserver.py ") + docroot);
+var docroot, base_url, js_base_url;
+
+juice.build.config.load();
+docroot     = juice.build.final_file_path('');
+base_url    = juice.url.make(juice.build.site_settings().base_url);
+js_base_url = juice.url.make(juice.build.site_settings().js_base_url);
+
+juice.sys.system(juice.path_join(juice.home(), "tools/runserver.py ") +
+                 [docroot,
+                  base_url.host, base_url.port,
+                  js_base_url.host, js_base_url.port].join(" "));
--- tools/runserver.py
+++ tools/runserver.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python
+
+import BaseHTTPServer
+import SimpleHTTPServer
 import os
+import signal
 import sys
+import thread
 
-class UsageError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+def run_server(server_address):
+    httpd = BaseHTTPServer.HTTPServer(server_address, SimpleHTTPServer.SimpleHTTPRequestHandler)
+    hostname, port = httpd.socket.getsockname()
+    print "Serving HTTP on", hostname, "port", port, "..."
+    httpd.serve_forever()
 
-def run_server(args):
-    os.chdir(args[1])
-    import SimpleHTTPServer
-    # hack to make SimpleHTTPServer work
-    sys.argv = []
-    SimpleHTTPServer.test()
+def main(args):
+    args = args[1:]
+    document_root, hostname, port, js_hostname, js_port = args
+
+    os.chdir(document_root)
+    base_server_address = (hostname, int(port))
+    js_server_address   = (hostname, int(js_port))
+
+    SimpleHTTPServer.SimpleHTTPRequestHandler.protocol_version = "HTTP/1.0"
+
+    base = thread.start_new_thread(run_server, (base_server_address,))
+
+    if js_server_address != base_server_address:
+        js = thread.start_new_thread(run_server, (js_server_address,))
+
+    signal.signal(signal.SIGINT, lambda signum, stackframe: thread.exit())
+
+    while True:
+        pass
 
 if __name__ == '__main__':
-    run_server(sys.argv)
+    sys.exit(main(sys.argv))
