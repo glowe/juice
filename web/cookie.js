633576a13e5364270ecc4062442637b2ee749015
Daniel Cowgill <dcowgill@masamune.localdomain>
2009-06-15 17:18:28 -0400

    Fixed documentation in cookie utility lib.

--- web/cookie.js
+++ web/cookie.js
@@ -4,18 +4,24 @@
 //
 
 juice.cookie = {
+
     // Returns the value of cookie if it exists, null otherwise.
+
     get: function(name) {
         var results = document.cookie.match("(?:^|;) ?" + name + "=([^;]*)(?:;|$)");
         return juice.is_null(results) ? null : unescape(results[1]);
     },
 
-    // Sets the specified ession cookie.
+    // Sets the specified session cookie. NOTE: `value` will be converted into
+    // a string, e.g. false will become String("false"). Corollary: to set an
+    // empty cookie, use an empty string (not null or false or undefined).
+
     set: function(name, value) {
         document.cookie = name + "=" + escape(value) + "; path=/";
     },
 
     // Deletes the specified cookie.
+
     remove: function(name) {
         document.cookie = name + "=; path=/; expires=" + (new Date(0)).toGMTString();
     }
